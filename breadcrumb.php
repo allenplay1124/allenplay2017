d7c6f12dba3e987c9cc0df89a19b2226ac4a85b1
allenplay <jailin1124@gmail.com>
2017-02-01 00:47:46 +0800

    修正麵包屑bug

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,26 +1,10 @@
 <?php global $post; ?>
 <ol class="breadcrumb">
-    <li><a href="<?php echo get_bloginfo('url') ?>"><?php echo get_bloginfo('name') ?></a></li>
-    <?php if (is_single()): ?>
-        <?php foreach (get_the_category() as $val): ?>
-            <li><a href="<?php get_category_link($val->term_id) ?>"><?php echo $val->cat_name ?></a></li>
-        <?php endforeach; ?>
-        <li class="active">
-            <?php if (is_category()): ?>
-                <?php single_cat_title('', false); ?>
-            <?php elseif (is_tag()): ?>
-                <?php single_tag_title('', false); ?>
-            <?php elseif (is_day()): ?>
-                <?php get_the_time(get_option('date_format')); ?>
-            <?php elseif (is_month()): ?>
-                <?php get_the_time('F, Y'); ?>
-            <?php elseif (is_year()): ?>
-                <?php get_the_time('Y') ?>
-            <?php elseif (is_page()): ?>
-                <?php echo $post->post_title ?>
-            <?php else: ?>
-                <?php echo $post->post_title  ?>
-            <?php endif; ?>
-        </li>
+    <?php if (!is_home()): ?>
+        <li><a href="<?php echo get_option('home') ?>"><?php echo bloginfo('name') ?></a></li>
     <?php endif; ?>
+    <?php if(is_category() || is_single()): ?>
+        <li><?php the_category('title_li=') ?></li>
+    <?php endif; ?>
+    <li class="active"><?php the_title(); ?></li>
 </ol>
