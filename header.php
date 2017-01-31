529cd95bcbba6f65413038c5495d495f66463cf2
allenplay <jailin1124@gmail.com>
2017-02-01 00:48:32 +0800

    加入 og:meta

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <?php include_once 'wp_bootstrap_navwalker.php'; ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php (is_singular() && get_option('thread_comments')) ? wp_enqueue_script('comment-reply') : ''; ?>
-    <?php wp_head(); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
     <title><?php
             if (is_home()) {
                 bloginfo('name');
@@ -21,19 +21,40 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>?20170102">
-    
+
+    <?php if(is_single()): ?>
+        <?php global $post ?>
+        <?php the_post(); ?>
+
+        <meta name="author" content="<?php echo get_the_author_meta('display_name') ?>">
+        <meta name="copyright" content="<?php echo bloginfo('name') ?>">
+        <meta itemprop="name" content="<?php wp_title(' - ', true, 'right'); ?><?php bloginfo('name'); ?>">
+        <?php if(has_post_thumbnail()): ?>
+            <meta itemprop="image" content="<?php echo get_img_url(the_ID()) ?>">
+        <?php endif; ?>
+
+        <meta property="og:title" content="<?php wp_title(' - ', true, 'right'); ?><?php bloginfo('name'); ?>" >
+        <meta property="og:url" content="<?php echo $post->guid ?>">
+        <meta property="og:image" content="<?php echo get_img_url(the_ID()) ?>">
+
+    <?php endif; ?>
+
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
+    <link rel="shortcut icon" href="<?php bloginfo('template_url');?>/favicon.ico" />
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>?20170102">
 </head>
 
 <body>
@@ -44,35 +65,35 @@
             <ul class="top-nav-ul">
                 <?php if (isset($top_bar['facebook']) && $top_bar['facebook'] != ''): ?>
                     <li>
-                        <a href="<?php echo $top_bar['facebook']; ?>">
+                        <a target="_blank" href="<?php echo $top_bar['facebook']; ?>">
                             <i class="fa fa-facebook"></i>
                         </a>
                     </li>
                 <?php endif; ?>
                 <?php if (isset($top_bar['instagram']) && $top_bar['instagram'] != ''): ?>
                     <li>
-                        <a href="<?php echo $top_bar['instagram']?>">
+                        <a target="_blank" href="<?php echo $top_bar['instagram']?>">
                             <i class="fa fa-instagram"></i>
                         </a>
                     </li>
                 <?php endif; ?>
                 <?php if (isset($top_bar['twitter']) && $top_bar['twitter'] != ''): ?>
                     <li>
-                        <a href="<?php echo $top_bar['twitter']?>">
+                        <a target="_blank" href="<?php echo $top_bar['twitter']?>">
                             <i class="fa fa-twitter"></i>
                         </a>
                     </li>
                 <?php endif; ?>
                 <?php if (isset($top_bar['google']) && $top_bar['google'] != ''): ?>
                      <li>
-                        <a href="<?php echo $top_bar['google']?>">
+                        <a target="_blank" href="<?php echo $top_bar['google']?>">
                             <i class="fa fa-google-plus"></i>
                         </a>
                     </li>
                 <?php endif;?>
                 <?php if (isset($top_bar['youtube']) && $top_bar['youtube'] != ''): ?>
                      <li>
-                        <a href="<?php echo $top_bar['Youtube']?>">
+                        <a target="_blank" href="<?php echo $top_bar['Youtube']?>">
                             <i class="fa fa-youtube-play"></i>
                         </a>
                     </li>
