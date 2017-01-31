78337783bf266fbf5f810295409640a3c88650fa
allenplay <jailin1124@gmail.com>
2017-02-01 00:47:25 +0800

    修改首頁樣版

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 get_header();
 include_once 'slider.php';
+global $wp_query, $wp_rewrite;
+$current = $wp_query->query_vars['paged'];
 
-$article = get_posts(array('numberposts' => 9));
+$article = get_posts(array('posts_per_page' => 9, 'offset' => $current));
 $top_bar = get_option('top_bar_option');
 foreach ($article as $key => $val) {
     $article[$key] = get_object_vars($val);
@@ -39,154 +41,9 @@ foreach ($article as $key => $val) {
                 </a>
             <?php endforeach; ?>
         </div>
+        <?php include_once('page_nav.php'); ?>
     </section>
-    <?php if (isset($top_bar['second_cate']) && $top_bar['second_cate'] != ''): ?>
-        <?php $second_cate = get_posts(array('numberposts' => 4, 'category' => $top_bar['v'])); ?>
-        <?php foreach ($second_cate as $key => $val): ?>
-            <?php $second_cate[$key] = get_object_vars($val); ?>
-        <?php endforeach; ?>
-        <section id="second_cate">
-            <div class="container">
-                <div class="page-header">
-                    <?php $second_cate_title = get_category($top_bar['second_cate'], ARRAY_A); ?>
-                    <h2>
-                        <span class="section-title">
-                            <?php echo $second_cate_title['cat_name'] ?>
-                        </span>
-                        <small><?php echo $second_cate_title['category_description'] ?></small>
-                    </h2>
-                </div>
-                <?php foreach ($second_cate as $val): ?>
-                    <a href="<?php echo wp_get_shortlink($val['ID']) ?>">
-                        <article class="col-sm-12 col-md-10 col-lg-5 post-content">
-                            <?php if (has_post_thumbnail($val['ID'])): ?>
-                                <div class="col-sm-6 post-thumb-img" style="
-                                    background:url('<?php echo get_img_url($val['ID']); ?>');
-                                    background-size: cover;
-                                    background-attachment: fixed;
-                                    background-position: center;
-                                    background-repeat: no-repeat;
-                                    height:250px;
-                                ">
-                                </div>
-                            <?php endif; ?>
-                            <div class="col-sm-6">
-                                <div class="post-title"><?php echo $val['post_title'] ?></div>
-                                <div class="post-meta">
-                                    <i class="glyphicon glyphicon-time"></i>&nbsp;
-                                    <?php echo DATE('Y-m-d', strtotime($val['post_date'])) ?>&nbsp;
-                                    <i class="glyphicon glyphicon-comment"></i>&nbsp;
-                                    <?php $comment = get_comment_count($val['ID']) ?>
-                                    <?php echo $comment['approved'] ?>
-                                </div>
-                                <div class="content">
-                                    <?php echo mb_substr(strip_tags($val['post_content']), 0, 100, 'utf-8');?>
-                                    ...
-                                </div>
-                            </div>
-                        </article>
-                    </a>
-                <?php endforeach; ?>
-            </div>
-        </section>
-    <?php endif; ?>
-    <?php if (isset($top_bar['first_cate']) && $top_bar['first_cate'] != ''): ?>
-        <?php $first_cate = get_posts(array('numberposts' => 4, 'category' => $top_bar['first_cate'])); ?>
-        <?php foreach ($first_cate as $key => $val): ?>
-            <?php $first_cate[$key] = get_object_vars($val); ?>
-        <?php endforeach; ?>
-        <section id="first_cate">
-            <div class="container">
-                <div class="page-header">
-                    <?php $first_cate_title = get_category($top_bar['first_cate'], ARRAY_A); ?>
-                    <h2>
-                        <span class="section-title">
-                        <?php echo $first_cate_title['cat_name'] ?>
-                        </span>
-                      <small><?php echo $first_cate_title['category_description'] ?></small>
-                    </h2>
-                </div>
-                <?php foreach ($first_cate as $val): ?>
-                    <a href="<?php echo wp_get_shortlink($val['ID']) ?>">
-                        <article class="col-sm-12 col-md-6 col-lg-3">
-                            <?php if (has_post_thumbnail($val['ID'])): ?>
-                                <div class="post-thumb-img" style="
-                                    background:url('<?php echo get_img_url($val['ID']); ?>');
-                                    background-size: cover;
-                                    background-attachment: fixed;
-                                    background-position: center;
-                                    background-repeat: no-repeat;
-                                    width:100%;
-                                    height:250px;
-                                    -webkit-border-radius: 500px;
-                                    -moz-border-radius: 500px;
-                                    border-radius: 500px;
-                                ">
-                                </div>
-                            <?php endif; ?>
-                            <div class="post-title">
-                                <?php echo $val['post_title'] ?>
-                            </div>
-                        </article>
-                    </a>
-                <?php endforeach; ?>
-            </div>
-        </section>
-    <?php endif; ?>
 
-    <?php if (isset($top_bar['third_cate']) && $top_bar['third_cate'] != ''): ?>
-        <?php $third_cate = get_posts(array('numberposts' => 4, 'category' => $top_bar['third_cate'])); ?>
-        <?php foreach ($third_cate as $key => $val): ?>
-            <?php $third_cate[$key] = get_object_vars($val); ?>
-        <?php endforeach; ?>
-        <section id="third_cate">
-            <div class="container">
-                <div class="page-header">
-                    <?php $third_cate_title = get_category($top_bar['third_cate'], ARRAY_A); ?>
-                    <h2>
-                        <span class="section-title">
-                            <?php echo $third_cate_title['cat_name'] ?>
-                        </span>
-                        <small><?php echo $third_cate_title['category_description'] ?></small>
-                    </h2>
-                </div>
-                <?php foreach ($third_cate as $key => $val): ?>
-                    <a href="<?php echo wp_get_shortlink($val['ID']) ?>">
-                        <article class="col-sm-12 post-content">
-                            <?php if (has_post_thumbnail($val['ID'])): ?>
-                                <div class="post-thumb-img"  style="
-                                    background:url('<?php echo get_img_url($val['ID']); ?>');
-                                    background-size: cover;
-                                    background-attachment: fixed;
-                                    background-position: center;
-                                    background-repeat: no-repeat;
-                                    width:100%;
-                                    height:480px;
-                                ">
-                                </div>
-                            <?php endif; ?>
-                            <div class="post-data-<?php echo ($key % 2) ? 'odd' : 'even'; ?>">
-                                <div class="post-title">
-                                    <?php echo $val['post_title'] ?>
-                                </div>
-                                <div class="post-meta">
-                                    <i class="glyphicon glyphicon-time"></i>&nbsp;
-                                    <?php echo DATE('Y-m-d', strtotime($val['post_date'])) ?>&nbsp;
-                                    <i class="glyphicon glyphicon-comment"></i>&nbsp;
-                                    <?php $comment = get_comment_count($val['ID']) ?>
-                                    <?php echo $comment['approved'] ?>
-                                </div>
-                                <div class="content">
-                                    <?php echo mb_substr(strip_tags($val['post_content']), 0, 100, 'utf-8');?>
-                                    ...
-                                </div>
-                            </div>
-                        </article>
-                    </a>
-                <?php endforeach; ?>
-            </div>
-        </section>
-    <?php endif; ?>
 </main>
 
 <script type="text/javascript">
