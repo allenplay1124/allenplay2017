b676a0275c9cfa5e7e21ce060946244dbe1ff310
allenplay <jailin1124@gmail.com>
2017-02-01 00:48:08 +0800

    加入 wp_footer()

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
     <div class="container">
         <?php $top_bar = get_option('top_bar_option'); ?>
         <?php echo (isset($top_bar['footer'])) ? $top_bar['footer'] : ''; ?>
+        <?php wp_footer(); ?>
     </div>
 </footer>
